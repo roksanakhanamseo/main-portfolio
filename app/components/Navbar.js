ed0e43b99bdf6626dc1b1a1ad064015747f10b96
roksana <roksanakhanamseo@gmail.com>
2024-10-13 04:24:28 +0600

    hollla

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,18 @@
+"use client";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
+
+// const downloadFileAtURL = () => {
+//   const url = "/tanvir.pdf"; // replace with your file path or URL
+//   const aTag = document.createElement("a");
+//   aTag.href = url;
+//   aTag.setAttribute("download", "/tanvir.pdf");
+//   document.body.appendChild(aTag);
+//   aTag.click();
+//   aTag.remove();
+// };
 const Navbar = () => {
   return (
     <div className="sm:h-20 h-14 left-8 select-none sm:left-16 fixed top-0 z-20 bg-[#111] backdrop-blur-xl bg-opacity-30 w-full">
@@ -21,9 +32,13 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <button className="rounded-2xl flex-end absolute sm:right-32 right-16 top-2 md:top-3  border-2  border-green-400 bg-green-400 px-3 py-1 sm:px-6 sm:py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
+      <a
+        href="/tanvir.pdf"
+        target="_blank"
+        className="rounded-2xl flex-end absolute sm:right-32 right-16 top-2 md:top-3  border-2  border-green-400 bg-green-400 px-3 py-1 sm:px-6 sm:py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none"
+      >
         My Resume
-      </button>
+      </a>
     </div>
   );
 };
