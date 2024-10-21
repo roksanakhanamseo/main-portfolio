fc516fb5fea87c169d6fcb38a1a2ff1b50caf3df
roksana khanam <roksanakhanamseo@gmail.com>
2024-10-22 02:35:50 +0600

    hoal

--- app/components/LogoBox.js
+++ app/components/LogoBox.js
@@ -7,7 +7,7 @@ const LogoBox = () => {
         draggable="true"
         className="h-10 w-10  cursor-pointer p-2 select-none  rounded-lg flex justify-center items-center bg-[#111] "
       >
-        <div className="text-3xl">T</div>
+        <div className="text-3xl">R</div>
         <div className="text-3xl  text-green-300">.</div>
       </Link>
     </div>
